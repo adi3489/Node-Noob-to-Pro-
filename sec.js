5621fddf6fd96c4173dce92db188077d5fc7ecea
adi3489 <07aditya.cn@gmail.com>
2024-09-12 12:41:33 +0530

    Server

--- sec.js
+++ sec.js
@@ -1,1 +1,5 @@
 console.log("very imp js code");
+//
+(function () {
+  // all the code of the module runs insider here
+})();
---
+++ sync.js
@@ -0,0 +1,13 @@
+console.log("Hello World");
+
+var a = 1078698;
+var b = 20986;
+
+function multiply(x, y) {
+  const result = a * b;
+  return result;
+}
+
+var c = multiply(a, b);
+
+console.log("multiply result is:", c);
